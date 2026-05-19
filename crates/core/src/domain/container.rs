52cdb1fee422c7ce4712bda714e8ce64b2cebbeb
Kronborg <mkronborg7@gmail.com>
2026-05-19 17:11:19 +0200

    start on the docker crate

---
+++ crates/core/src/domain/container.rs
@@ -0,0 +1,46 @@
+use crate::error::container::{self, ContainerError};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ContainerRestartPolicy {
+    EMPTY,
+    NO,
+    ALWAYS,
+    UNLESS_STOPPED,
+    ON_FAILURE,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Container {
+    id: String,
+    name: String,
+    restart_policy: ContainerRestartPolicy,
+}
+
+impl Container {
+    pub fn new(
+        id: String,
+        name: String,
+        restart_policy: ContainerRestartPolicy,
+    ) -> Result<Self, ContainerError> {
+        if id.is_empty() || name.is_empty() {
+            return Err(ContainerError::CreateionError);
+        }
+
+        Ok(Self {
+            id,
+            name,
+            restart_policy,
+        })
+    }
+
+    pub fn id(&self) -> &String {
+        &self.id
+    }
+
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+    pub fn restart_policy(self) -> ContainerRestartPolicy {
+        self.restart_policy
+    }
+}
---
+++ crates/docker/src/client.rs
@@ -0,0 +1,10 @@
+use bollard::Docker;
+
+use crate::error::DockerError;
+
+pub fn connect() -> Result<Docker, DockerError> {
+    match Docker::connect_with_unix_defaults() {
+        Ok(docker) => Ok(docker),
+        Err(_) => Err(DockerError::ConncationError),
+    }
+}
