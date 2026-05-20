815cca1275c7ba67a74aa82f105501c3649140bd
Kronborg <mkronborg7@gmail.com>
2026-05-20 20:41:51 +0200

    chatGPT says this is cleaner version and i like it
    in this version we don't need af if else and need to docker becuse ehither is option none her or we can a filter becus of map

--- crates/docker/src/container.rs
+++ crates/docker/src/container.rs
@@ -8,22 +8,17 @@ pub async fn get_all_containers(
     docker: &Docker,
     option: Option<HashMap<String, Vec<String>>>,
 ) -> Result<Vec<ContainerSummary>, DockerError> {
-    if let Some(filter) = option {
-        docker
-            .list_containers(Some(
-                bollard::query_parameters::ListContainersOptionsBuilder::default()
-                    .all(true)
-                    .filters(&filter)
-                    .build(),
-            ))
-            .await
-            .map_err(|_| DockerError::ConnectionError)
-    } else {
-        docker
-            .list_containers(None)
-            .await
-            .map_err(|_| DockerError::ConnectionError)
-    }
+    let option = option.map(|filter| {
+        bollard::query_parameters::ListContainersOptionsBuilder::default()
+            .all(true)
+            .filters(&filter)
+            .build()
+    });
+
+    docker
+        .list_containers(option)
+        .await
+        .map_err(|_| DockerError::ConnectionError)
 }
 
 #[cfg(test)]
