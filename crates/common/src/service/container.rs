1a93070846775e457ac8cf44b421ee7eb96ad17e
Kronborg <mkronborg7@gmail.com>
2026-05-25 16:47:07 +0200

    upadted the test to use the service and not the function mong runtime

--- crates/common/src/service/container.rs
+++ crates/common/src/service/container.rs
@@ -24,22 +24,49 @@ where
     where
         'service: 'future,
     {
-        Box::pin(async move { self.runtime.containers().await })
+        Box::pin(async move {
+            self.runtime
+                .containers(None)
+                .await
+                .map_err(|error| ContainerError::FetchingError(error.to_string()))
+        })
+    }
+    pub fn get<'service, 'locator, 'future>(
+        &'service self,
+        locator: &'locator str,
+    ) -> Pin<Box<dyn Future<Output = Result<Option<Container>, ContainerError>> + Send + 'future>>
+    where
+        'service: 'future,
+        'locator: 'future,
+        Self: 'future,
+    {
+        Box::pin(async move {
+            if locator.is_empty() {
+                return Err(ContainerError::InvaldeLocator);
+            }
+            self.runtime
+                .get(locator)
+                .await
+                .map_err(|error| ContainerError::FetchingError(error.to_string()))
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
 
+    use crate::error::runtime::RuntimeError;
+
     use super::*;
-    use std::pin::Pin;
+    use std::{collections::HashMap, pin::Pin};
 
     struct MockRuntime;
 
     impl ContainerRuntime for MockRuntime {
         fn containers<'service, 'future>(
             &'service self,
-        ) -> Pin<Box<dyn Future<Output = Result<Vec<Container>, ContainerError>> + Send + 'future>>
+            _filter: Option<HashMap<String, Vec<String>>>,
+        ) -> Pin<Box<dyn Future<Output = Result<Vec<Container>, RuntimeError>> + Send + 'future>>
         where
             'service: 'future,
         {
@@ -48,47 +75,60 @@ mod tests {
 
         fn get<'service, 'locator, 'future>(
             &'service self,
-            locator: &'locator str,
-        ) -> Pin<Box<dyn Future<Output = Result<Option<Container>, ()>> + Send + 'future>>
+            _locator: &'locator str,
+        ) -> Pin<Box<dyn Future<Output = Result<Option<Container>, RuntimeError>> + Send + 'future>>
         where
             'service: 'future,
             'locator: 'future,
             Self: 'future,
         {
-            Box::pin(async move {
-                if locator.is_empty() {
-                    return Err(());
-                }
-                Ok(None)
-            })
+            Box::pin(async move { Ok(None) })
         }
         fn update_restart_policy<'a>(
             &'a self,
-            locator: &'a str,
-        ) -> Pin<Box<dyn Future<Output = Result<Option<Container>, ()>> + Send + 'a>> {
+            _locator: &'a str,
+        ) -> Pin<Box<dyn Future<Output = Result<Option<Container>, RuntimeError>> + Send + 'a>>
+        {
             todo!()
         }
         fn update_state<'a>(
             &'a self,
-            locator: &'a str,
-        ) -> Pin<Box<dyn Future<Output = Result<Container, ()>> + Send + 'a>> {
+            _locator: &'a str,
+        ) -> Pin<Box<dyn Future<Output = Result<Option<Container>, RuntimeError>> + Send + 'a>>
+        {
             todo!()
         }
 
         fn shoutdown<'a>(
             &'a self,
-            locator: &'a str,
-        ) -> Pin<Box<dyn Future<Output = Result<Option<bool>, ()>> + Send + 'a>> {
+            _locator: &'a str,
+        ) -> Pin<Box<dyn Future<Output = Result<bool, RuntimeError>> + Send + 'a>> {
             todo!()
         }
     }
 
     fn service() -> ContainerService<MockRuntime> {
-        ContainerService::new(MockRuntime)
+        ContainerService::new(Arc::new(MockRuntime))
     }
 
     #[tokio::test]
     async fn get_by_emty_name() {
+        let service = service();
+
+        let result = service.get("").await;
+
+        assert!(result.is_err());
+    }
+    #[tokio::test]
+    async fn get_by_vailed_locator() {
+        let service = service();
+
+        let result = service.get("ASGLK").await;
+
+        assert!(result.is_ok());
+    }
+    #[tokio::test]
+    async fn get_by_invailed_locator() {
         let service = service();
 
         let result = service.runtime.get("").await;
@@ -96,19 +136,10 @@ mod tests {
         assert!(result.is_err());
     }
     #[tokio::test]
-    async fn get_by_locator() {
-        let service = service();
-
-        let result = service.runtime.get("ASGLK").await;
-
-        assert!(result.is_ok());
-    }
-
-    #[tokio::test]
     async fn container() {
         let service = service();
 
-        let result = service.runtime.containers().await;
+        let result = service.runtime.containers(None).await;
 
         assert!(result.is_ok());
     }
