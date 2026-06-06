30677bb16f4469321e018f432d2af7efd5014626
Mikkel Kronborg <mkronborg7@gmail.com>
2026-06-06 19:57:23 +0200

    add back the sub if the option is smaller then target

--- crates/fuzzy/src/main.rs
+++ crates/fuzzy/src/main.rs
@@ -4,9 +4,14 @@ fn main() {
     //     vec!["hello", "kronborg", "he", "alllll", "helllo", "olleh"],
     // );
     // finder("hel", vec!["hello", "kronborg", "he", "helllo", "olleh"]);
-    let gg = seach_list("hel", vec!["hello", "kronborg", "he", "helllo", "olleh"]);
 
-    println!("{:?}", gg);
+    let target = "dough-db";
+    let gg = seach_list(
+        target,
+        vec!["he", "hello", "kronborg", "he", "helllo", "olleh"],
+    );
+
+    println!("target: {target}\n{:?}", gg);
 }
 
 // #[derive(Debug, Clone)]
@@ -15,41 +20,6 @@ fn main() {
 //     match_project: u8,
 // }
 
-// fn seach_list<'a>(target: &str, options: Vec<&'a str>) -> Result<Vec<(&'a str, u8)>, ()> {
-// fn finder(target: &str, options: Vec<&str>) {
-//     let target_bytes: &[u8] = target.as_bytes();
-//     let target_len = target_bytes.len();
-//     let mut one_to_one = true;
-//
-//     for name in options {
-//         let mut result = Res {
-//             name: name.to_string(),
-//             match_project: 100,
-//         };
-//         let option_byte: &[u8] = name.as_bytes();
-//
-//         for (index, value) in option_byte.iter().enumerate() {
-//             if index < target_bytes.len() && target_bytes[index] == *value {
-//             } else if target_bytes.contains(value) {
-//                 result.match_project -= 3;
-//             } else {
-//                 result.match_project -= 5;
-//             }
-//         }
-//
-//         if option_byte.len() < target_len {
-//             result.match_project -= (u8::try_from(target_len).unwrap_or(0)
-//                 - u8::try_from(name.len()).unwrap_or(0))
-//                 * 10;
-//         }
-//         println!(
-//             "name: {0:?} match: {1:?}",
-//             result.name, result.match_project
-//         );
-//     }
-// }
-
-// fn seach_list<'a>(target: &str, options: Vec<&'a str>) -> Result<Vec<(&'a str, u8)>, ()> {
 fn seach_list<'a>(target: &str, options: Vec<&'a str>) -> Vec<(&'a str, u8)> {
     options
         .iter()
@@ -70,6 +40,8 @@ pub fn compare<'a>(target: &str, option: &'a str) -> (&'a str, u8) {
     for (index, value) in option_bytes.iter().enumerate() {
         if index < target_len {
             if target_bytes[index] == *value {
+                println!("{target}, {option}");
+                println!("match: {}, {value}", target_bytes[index]);
                 no_match = false;
             } else if target_bytes.contains(value) {
                 one_to_one = false;
@@ -80,6 +52,10 @@ pub fn compare<'a>(target: &str, option: &'a str) -> (&'a str, u8) {
             one_to_one = false;
             match_score -= 6;
         }
+    }
+    if option_bytes.len() < target_len {
+        match_score -=
+            (u8::try_from(target_len).unwrap_or(0) - u8::try_from(option.len()).unwrap_or(0)) * 10;
     }
 
     if no_match {
